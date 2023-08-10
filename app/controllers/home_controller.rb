94e438a030d8a5e4102f0b7e815a34d2e5199bc9
Lewis Youl <lewis.youl@trustflight.com>
2023-08-10 16:58:38 +0100

    feat: add more scopes so you don't have to manually authorize each time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     redirect_to authenticated_root_path if current_user
 
-    @strava_login_url = "https://www.strava.com/oauth/authorize?client_id=57045&scope=activity:read&redirect_uri=#{base_env_url}/oauth&response_type=code&approval_prompt=auto&state=private"
+    @strava_login_url = "https://www.strava.com/oauth/authorize?client_id=57045&scope=read,read_all,profile:read_all,profile:write,activity:read,activity:read_all,activity:write&redirect_uri=#{base_env_url}/oauth&response_type=code&approval_prompt=auto&state=private"
   end
 end
