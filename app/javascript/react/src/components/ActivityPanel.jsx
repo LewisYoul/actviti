65682ca0e6ea04329f6cf549a912b02906fe4bd3
Lewis Youl <lewis.youl@trustflight.com>
2024-04-06 12:34:13 +0100

    feat: add loading spinner

--- app/javascript/react/src/components/ActivityPanel.jsx
+++ app/javascript/react/src/components/ActivityPanel.jsx
@@ -33,7 +33,14 @@ const ActivityPanel = ({ activity }) => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
         </button>
-      {!localActivity && (<div>Loading...</div>)}
+      {!localActivity && (
+        <svg xmlns="http://www.w3.org/2000/svg" class="block m-auto" width="50px" height="50px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+          <circle cx="50" cy="50" fill="none" stroke="#8140b6" strokeWidth="10" r="35" strokeDasharray="164.93361431346415 56.97787143782138">
+            <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1">
+            </animateTransform>
+          </circle>
+        </svg>
+      )}
       {localActivity && (
         <div>
           <div className="flex items-start justify-between">
