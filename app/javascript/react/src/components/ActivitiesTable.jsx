abf878e942580890bea56f646a7ead056ef154dc
Lewis Youl <lewis.youl@trustflight.com>
2024-04-16 08:39:38 +0100

    feat: show slider filters properly on mobile

--- app/javascript/react/src/components/ActivitiesTable.jsx
+++ app/javascript/react/src/components/ActivitiesTable.jsx
@@ -18,28 +18,21 @@ const ActivitiesTable = ({ activities, selectedActivity }) => {
       <div className="flex flex-col flex-none h-full">
         <div className="w-full flex bg-white">
           <div className="whitespace-nowrap overflow-hidden py-2 pl-4 pr-3 text-left text-xs md:text-sm font-medium text-gray-400 sm:pl-6 w-4/12">NAME</div>
-          <div className="whitespace-nowrap overflow-hidden px-2 py-2 text-left text-xs md:text-sm font-medium text-gray-400 w-2/12">DATE</div>
+          <div className="whitespace-nowrap overflow-hidden px-2 py-2 text-left text-xs md:text-sm font-medium text-gray-400 w-3/12 md:w-2/12">DATE</div>
           <div className="whitespace-nowrap overflow-hidden px-2 py-2 text-left text-xs md:text-sm font-medium text-gray-400 w-2/12">TYPE</div>
           <div className="whitespace-nowrap overflow-hidden px-2 py-2 text-left text-xs md:text-sm font-medium text-gray-400 w-2/12">DIST (km)</div>
           <div className="hidden md:block whitespace-nowrap overflow-hidden px-2 py-2 pr-4 text-left text-xs md:text-sm font-medium text-gray-400 w-2/12">TIME</div>
-          <div className="whitespace-nowrap overflow-hidden px-2 py-2 pr-4 text-left text-xs md:text-sm font-medium text-gray-400 hidden"></div>
         </div>
         <div className="flex-1 overflow-auto">
           {
             activities.map(activity => {
               return (
-                <div key={activity.id} onClick={() => { toggleActivity(activity) }} className={`flex cursor-pointer ${activity.id === selectedActivity?.id ? 'bg-purple-200' : 'odd:bg-gray-50 hover:bg-purple-100'}`}>
-                  <div className="whitespace-nowrap overflow-hidden py-1.5 pl-4 pr-3 text-xs md:text-sm text-gray-900 sm:pl-6 w-4/12">{ activity.name() }</div>
-                  <div className="whitespace-nowrap overflow-hidden px-2 py-1.5 text-xs md:text-sm text-gray-900 w-2/12">{ activity.startDateShort() }</div>
-                  <div className="whitespace-nowrap overflow-hidden px-2 py-1.5 text-xs md:text-sm text-gray-900 text-left w-2/12">{ activity.type() }</div>
-                  <div className="whitespace-nowrap overflow-hidden px-2 py-1.5 text-xs md:text-sm text-left text-gray-900 w-2/12">{ activity.distance() }</div>
-                  <div className="hidden md:block whitespace-nowrap overflow-hidden px-2 pr-4 py-1.5 text-xs md:text-sm text-left text-gray-900 w-2/12">{ activity.movingTime() }</div>
-                  <div className="hidden">
-                    {/* <%= button_to('', { controller: :activities, action: :select, id: activity.id }, { id: "select_activity_row_#{activity.id}", data: { turbo: true }, method: :put }) %> */}
-                  </div>
-                  <div className="hidden">
-                    {/* <%= button_to('', { controller: :activities, action: :deselect, id: activity.id }, { id: "deselect_activity_row_#{activity.id}", data: { turbo: true }, method: :put }) %> */}
-                  </div>
+                <div key={activity.id} onClick={() => { toggleActivity(activity) }} className={`cursor-pointer ${activity.id === selectedActivity?.id ? 'bg-purple-200' : 'odd:bg-gray-50 hover:bg-purple-100'}`}>
+                  <div className="inline-block whitespace-nowrap overflow-hidden py-1.5 pl-4 pr-3 text-xs md:text-sm text-gray-900 sm:pl-6 w-4/12">{ activity.name() }</div>
+                  <div className="inline-block whitespace-nowrap overflow-hidden px-2 py-1.5 text-xs md:text-sm text-gray-900 w-3/12 md:w-2/12">{ activity.startDateShort() }</div>
+                  <div className="inline-block whitespace-nowrap overflow-hidden px-2 py-1.5 text-xs md:text-sm text-gray-900 text-left w-2/12">{ activity.type() }</div>
+                  <div className="inline-block whitespace-nowrap overflow-hidden px-2 py-1.5 text-xs md:text-sm text-left text-gray-900 w-2/12">{ activity.distance() }</div>
+                  <div className="hidden md:inline-block whitespace-nowrap overflow-hidden px-2 pr-4 py-1.5 text-xs md:text-sm text-left text-gray-900 w-2/12">{ activity.movingTime() }</div>
                 </div>
               )
             })
--- app/javascript/react/src/components/RangeSlider.jsx
+++ app/javascript/react/src/components/RangeSlider.jsx
@@ -26,8 +26,21 @@ const RangeSlider = ({ label, min, max, step, decimalPlaces, unit, onChange}) =>
 
   React.useEffect(() => {
     const slider = document.getElementById(`${label}-slider`)
+    const mobileSlider = document.getElementById(`${label}-mobile-slider`)
 
     noUiSlider.create(slider, {
+      start: [min, max],
+      connect: true,
+      range: {
+        'min': min,
+        'max': max
+      },
+      step: step,
+      behaviour: 'tap-drag',
+      format: wNumb({ decimals: decimalPlaces }),
+    });
+
+    noUiSlider.create(mobileSlider, {
       start: [min, max],
       connect: true,
       range: {
@@ -73,12 +86,65 @@ const RangeSlider = ({ label, min, max, step, decimalPlaces, unit, onChange}) =>
 
       onChange({ min: selectedMinimum, max: selectedMaximum })
     })
+
+    mobileSlider.noUiSlider.on('slide', (values) => {
+      const [rawMinimum, rawMaximum] = values
+      let [currentMinimum, currentMaximum] = values
+
+      if (unit === 'h') {
+        if (String(currentMinimum)[String(currentMinimum).length - 1] === '0') {
+
+          currentMinimum = parseInt(currentMinimum)
+        }
+
+        if (String(currentMaximum)[String(currentMaximum).length - 1] === '0') {
+          currentMaximum = parseInt(currentMaximum)
+        }
+      }
+
+      setMinValue(currentMinimum)
+      setMaxValue(currentMaximum)
+
+      if (rawMinimum == min && rawMaximum == max) {
+        setDisplayLabel(label)
+      } else if (rawMinimum == min) {
+        setDisplayLabel(`< ${currentMaximum}${unit}`)
+      } else if (rawMaximum == max) {
+        setDisplayLabel(`> ${currentMinimum}${unit}`)
+      } else {
+        setDisplayLabel(`${currentMinimum} - ${currentMaximum}${unit}`)
+      }
+    })
+    
+    mobileSlider.noUiSlider.on('change', (values) => {
+      const [selectedMinimum, selectedMaximum] = values
+
+      onChange({ min: selectedMinimum, max: selectedMaximum })
+    })
   }, [])
 
   return (
     <div ref={rangeSliderRef} className="flex items-center bg-white rounded-md ml-2 shadow-md relative">
       <div className="flex items-center p-2 cursor-pointer">
         <button onClick={() => setShowPopover(!showPopover)} className="text-xs">{displayLabel}</button>
+      </div>
+
+      <div onClick={() => setShowPopover(!showPopover)} className={`${showPopover ? 'md:hidden' : 'hidden'} z-500 top-0 left-0 fixed bg-gray-500 opacity-50 h-screen w-screen`}>
+        <div className="relative h-full w-full">
+          <div className="bg-white absolute opacity-none left-0 bottom-0 z-600 shadow-md md:rounded-md p-3 w-full">
+            <div>
+              <div className="flex justify-center">
+                <label htmlFor="distance">{label}</label>
+              </div>
+              <hr className="my-1"></hr>
+              <div className="flex justify-between mb-2">
+                <span>{`${minValue}${unit}`}</span>
+                <span>{`${maxValue}${unit}`}</span>
+              </div>
+              <div id={`${label}-mobile-slider`}></div>
+            </div>
+          </div>
+        </div>
       </div>
       
       <div className={showPopover ? '' : 'hidden'}>
